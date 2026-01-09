8567d37ac9762e09767c2891d9ad5e7bb6f988d4
Svetoslav Stoyanov <svetoslav.stoyanov@endava.com>
2026-01-09 14:44:07 +0200

    change to point to raw files

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-curl https://github.com/svetoslavstoyanov/dotfiles/blob/master/print.sh
+curl https://raw.githubusercontent.com/svetoslavstoyanov/dotfiles/refs/heads/master/print.sh
 source ./printh.sh
 
-curl https://github.com/svetoslavstoyanov/dotfiles/blob/master/create-user.sh
+curl https://raw.githubusercontent.com/svetoslavstoyanov/dotfiles/refs/heads/master/create-user.sh
 
 chmod +x ./create-user.sh
 ./create-user.sh
 
-curl https://github.com/svetoslavstoyanov/dotfiles/blob/master/setup-dev.sh
+curl https://raw.githubusercontent.com/svetoslavstoyanov/dotfiles/refs/heads/master/setup-dev.sh
 
 chmod +x ./setup-dev.sh
 
