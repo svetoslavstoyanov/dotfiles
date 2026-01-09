154f62f8132127daaa7df762185f6219305b94df
Svetoslav Stoyanov <svetoslav.stoyanov@endava.com>
2026-01-09 16:24:03 +0200

    fix typo

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 source <(curl -fsSL https://raw.githubusercontent.com/svetoslavstoyanov/dotfiles/refs/heads/master/print.sh)
 
-curl -fsSL https://raw.githubusercontent.com/svetoslavstoyanov/dotfiles/refs/heads/master/create-user.sh | bash
+curl -fsSL https://raw.githubusercontent.com/svetoslavstoyanov/dotfiles/refs/heads/master/setup-user.sh | bash
 curl -fsSL https://raw.githubusercontent.com/svetoslavstoyanov/dotfiles/refs/heads/master/setup-dev.sh | bash
