d2c0ad8823c0b9c2633e2e720c7957a8a3893d1c
Svetoslav Stoyanov <svetoslav.stoyanov@endava.com>
2026-01-09 16:01:54 +0200

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,4 @@
-source <(curl -fsSL https://raw.githubusercontent.com/svetoslavstoyanov/dotfiles/refs/heads/master/print.sh)>
+source <(curl -fsSL https://raw.githubusercontent.com/svetoslavstoyanov/dotfiles/refs/heads/master/print.sh)
 
 curl -fsSL https://raw.githubusercontent.com/svetoslavstoyanov/dotfiles/refs/heads/master/create-user.sh | bash
-
-
-#chmod +x ./create-user.sh
-#./create-user.sh
-
 curl -fsSL https://raw.githubusercontent.com/svetoslavstoyanov/dotfiles/refs/heads/master/setup-dev.sh | bash
-
-#chmod +x ./setup-dev.sh
-
-#rm -rf ./print.sh ./create-user.sh ./setup-dev.sh
