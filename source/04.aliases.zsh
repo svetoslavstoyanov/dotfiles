7113183a0f8e37dc22c72ac0d1fd2a3c1cc5d427
Svetoslav Stoyanov <svetoslav.stoyanov@endava.com>
2026-01-13 14:43:19 +0200

    fix alias

--- source/04.aliases.zsh
+++ source/04.aliases.zsh
@@ -3,7 +3,6 @@ alias lla="exa --long --header --icons --group-directories-first --sort=ext -a"
 alias glogdp="git log --stat --patch --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'\'"
 alias y="xclip"
 alias p="xclip -o"
-alias bat="batcat"
 alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 alias v=nvim
 alias cat="bat"
