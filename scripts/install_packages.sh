f2a1b7f5ce951bd054a435c316e1581135803096
Svetoslav Stoyanov <svetoslav39@gmail.com>
2026-08-28 12:09:53 +0000

    remove java installation

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -82,9 +82,4 @@ install_packages() {
   log "Installing .NET"
   curl -fsSL https://dot.net/v1/dotnet-install.sh | bash -s -- --channel LTS
 
-  # --- SDK_MAN AND JAVA ---
-  log "Installng sdkman"
-  curl -s "https://get.sdkman.io?sdkman_auto_answer=true&rcupdate=false" | bash
-  source "$HOME/.sdkman/bin/sdkman-init.sh"
-  sdk install java
 }
